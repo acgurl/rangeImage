8828649dc90d5820dc72dfea575119df3e7ddc93
ACGURL <1485942570@qq.com>
2025-01-06 12:55:15 +0000

    update

--- netlify/functions/rangeImage/rangeImage.js
+++ netlify/functions/rangeImage/rangeImage.js
@@ -1,23 +1,30 @@
-import type { Config, Context } from "@netlify/edge-functions";
-
-const images = [
-  "https://i0.hdslb.com/bfs/article/5e587e5511baf9c6366213d78a5468f73493083985480649.jpg",
-  "https://i0.hdslb.com/bfs/article/2aa2e85c44a13d1095f1c5fc10e7db653493083985480649.jpg",
-  "https://i0.hdslb.com/bfs/article/1e1042c7a0e7ace3e4c0d9c8d2dcbf1a3493083985480649.jpg",
-  "https://i0.hdslb.com/bfs/article/f3db656873a69fa6ba55e112d76f7e4f3493083985480649.png",
-  "https://i0.hdslb.com/bfs/article/faaf19210b1d07f8695884cd19d2da5e3493083985480649.png",
-  "https://i0.hdslb.com/bfs/article/766f1c08349fbd61c26a5be90b3d00843493083985480649.jpg",
-  "https://i0.hdslb.com/bfs/article/f561696ef41923c6e59f77aaf24c07383493083985480649.jpg",
-  "https://i0.hdslb.com/bfs/article/23299951dc1ca3ebc527e4a7c5dad9083493083985480649.png" 
-];
-
-
-export default async (req: Request, context: Context) => {
-  const randomImage = images[Math.floor(Math.random() * images.length)];
-  return Response.redirect(randomImage, 301);
+// randomImage.js  
+const { createHandler } = require('@netlify/functions');
+const fs = require('fs');
+const path = require('path');
+ 
+// 读取图片URL列表 
+const imageDirectory = path.join(process.cwd(),  'image_urls.json'); 
+const imageUrls = JSON.parse(fs.readFileSync(imageDirectory,  'utf8'));
+ 
+// 创建一个随机选择图片URL的函数 
+const getRandomImageUrl = () => {
+  const randomIndex = Math.floor(Math.random()  * imageUrls.length); 
+  return imageUrls[randomIndex];
 };
-
-
-export const config: Config = {
-  path: "/random-image"
-};
+ 
+// 边缘函数处理程序 
+const handler = createHandler((event, context) => {
+  // 从列表中随机选择一个图片URL 
+  const imageUrl = getRandomImageUrl();
+ 
+  // 返回301重定向响应 
+  return {
+    statusCode: 301,
+    headers: {
+      Location: imageUrl,
+    },
+  };
+});
+ 
+module.exports  = { handler };
