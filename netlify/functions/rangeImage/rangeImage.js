022a0a085f1486f7d253a6264373d359e3aece89
ACGURL <1485942570@qq.com>
2025-01-06 12:40:03 +0000

    update

--- netlify/functions/rangeImage/rangeImage.js
+++ netlify/functions/rangeImage/rangeImage.js
@@ -1,6 +1,7 @@
-import { Handler } from "@netlify/functions";
-
-// 假设这里有一个图片URL数组，你可以根据实际情况替换这些URL
+// 引入所需的模块 
+const { createHandler } = require('@netlify/functions');
+ 
+// 定义一个包含图片URLs的数组 
 const imageUrls = [
   "https://i0.hdslb.com/bfs/article/5e587e5511baf9c6366213d78a5468f73493083985480649.jpg",
   "https://i0.hdslb.com/bfs/article/2aa2e85c44a13d1095f1c5fc10e7db653493083985480649.jpg",
@@ -9,20 +10,34 @@ const imageUrls = [
   "https://i0.hdslb.com/bfs/article/faaf19210b1d07f8695884cd19d2da5e3493083985480649.png",
   "https://i0.hdslb.com/bfs/article/766f1c08349fbd61c26a5be90b3d00843493083985480649.jpg",
   "https://i0.hdslb.com/bfs/article/f561696ef41923c6e59f77aaf24c07383493083985480649.jpg",
-  "https://i0.hdslb.com/bfs/article/23299951dc1ca3ebc527e4a7c5dad9083493083985480649.png"
+  "https://i0.hdslb.com/bfs/article/23299951dc1ca3ebc527e4a7c5dad9083493083985480649.png" 
+  // 可以添加更多的图片URL 
 ];
-
-const handler: Handler = async (event, context) => {
-  const randomIndex = Math.floor(Math.random()  * imageUrls.length); 
-  const redirectUrl = imageUrls[randomIndex];
-
+ 
+// 创建一个边缘函数处理程序 
+const handler = createHandler(async (event) => {
+  // 从请求中获取URL路径参数 
+  const path = event.path; 
+  
+  // 检查路径是否符合预期，如果不是，则返回404 
+  if (path !== '/random-image') {
+    return {
+      status: 404,
+      body: 'Not Found',
+    };
+  }
+  
+  // 从imageUrls数组中随机选择一个图片URL 
+  const randomImageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)]; 
+  
+  // 返回301永久重定向响应到随机选择的图片URL 
   return {
-    statusCode: 301,
+    status: 301,
     headers: {
-      Location: redirectUrl
+      'Location': randomImageUrl,
     },
-    body: ""
   };
-};
-
-export { handler };
+});
+ 
+// 导出handler函数 
+module.exports  = { handler };
