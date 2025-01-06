20f95115f76cae7a4a6230cab78f6d7d43c6bdbe
ACGURL <1485942570@qq.com>
2025-01-06 13:14:34 +0000

    update

--- netlify/functions/rangeImage/rangeImage.js
+++ netlify/functions/rangeImage/rangeImage.js
@@ -3,38 +3,28 @@ const { createHandler } = require('@netlify/functions');
 const fs = require('fs');
 const path = require('path');
  
-// 获取当前工作目录 
-const currentDir = process.cwd(); 
+// 读取图片URL列表 
+const imageDirectory = path.join(process.cwd(),  'https://blog.acgurl.link/image_urls.json'); 
+const imageUrls = JSON.parse(fs.readFileSync(imageDirectory,  'utf8'));
  
-// 构建image_urls.json 的完整路径 
-const imageDirectory = path.join(currentDir,  'image_urls.json'); 
+// 创建一个随机选择图片URL的函数 
+const getRandomImageUrl = () => {
+  const randomIndex = Math.floor(Math.random()  * imageUrls.length); 
+  return imageUrls[randomIndex];
+};
  
-try {
-  // 尝试读取图片URL列表 
-  const imageUrls = JSON.parse(fs.readFileSync(imageDirectory,  'utf8'));
+// 边缘函数处理程序 
+const handler = createHandler((event, context) => {
+  // 从列表中随机选择一个图片URL 
+  const imageUrl = getRandomImageUrl();
  
-  // 创建一个随机选择图片URL的函数 
-  const getRandomImageUrl = () => {
-    const randomIndex = Math.floor(Math.random()  * imageUrls.length); 
-    return imageUrls[randomIndex];
+  // 返回301重定向响应 
+  return {
+    statusCode: 301,
+    headers: {
+      Location: imageUrl,
+    },
   };
+});
  
-  // 边缘函数处理程序 
-  const handler = createHandler((event, context) => {
-    // 从列表中随机选择一个图片URL 
-    const imageUrl = getRandomImageUrl();
- 
-    // 返回301重定向响应 
-    return {
-      statusCode: 301,
-      headers: {
-        Location: imageUrl,
-      },
-    };
-  });
- 
-  module.exports  = { handler };
-} catch (error) {
-  console.error('Error  reading image_urls.json:',  error);
-  throw error;
-}
+module.exports  = { handler };
